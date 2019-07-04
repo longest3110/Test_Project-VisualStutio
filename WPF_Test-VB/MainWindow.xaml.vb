47bc99224f7f0983694620f758c36aa6c7d08e62
Saito_Hiroki <longest3110@gmail.com>
2019-07-04 22:29:22 +0900

    [feature/issue_#2] とりあえずできた

--- WPF_Test-VB/MainWindow.xaml.vb
+++ WPF_Test-VB/MainWindow.xaml.vb
@@ -1,3 +1,21 @@
 ﻿Class MainWindow
+	Private Structure ListBoxThumbnail
+		Public Property ImagePath As String
+		Public Property Text As String
+	End Structure
+
+	Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+		Dim temp As ListBoxThumbnail
+		Dim datas As New List(Of ListBoxThumbnail)
+
+		temp = New ListBoxThumbnail()
+		temp.ImagePath = "C:\Users\longe\Develop\Test_Project-VisualStutio\WPF_Test-VB\Images\ハサミのフリーアイコン.png"
+		temp.Text = "ハサミ"
+		datas.Add(temp)
+
+
+		listBox.ItemsSource = datas
+	End Sub
+
 
 End Class
