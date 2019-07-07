0e86b2b53b02d5cb9869cd0406c33a753f853588
Saito_Hiroki <longest3110@gmail.com>
2019-07-07 11:00:44 +0900

    [feature/issue_#2] データソースを構造体からDataTableに

--- WPF_Test-VB/MainWindow.xaml.vb
+++ WPF_Test-VB/MainWindow.xaml.vb
@@ -1,21 +1,46 @@
-﻿Class MainWindow
-	Private Structure ListBoxThumbnail
-		Public Property ImagePath As String
-		Public Property Text As String
-	End Structure
+﻿Imports System.Data
+
+Class MainWindow
 
 	Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-		Dim temp As ListBoxThumbnail
-		Dim datas As New List(Of ListBoxThumbnail)
+		Dim dt As New DataTable
+		Dim row As DataRow
 
-		temp = New ListBoxThumbnail()
-		temp.ImagePath = "C:\Users\longe\Develop\Test_Project-VisualStutio\WPF_Test-VB\Images\ハサミのフリーアイコン.png"
-		temp.Text = "ハサミ"
-		datas.Add(temp)
+		dt.Columns.Add("Text")
+		dt.Columns.Add("ImagePath")
+
+		row = dt.NewRow
+		row("Text") = "さいとう"
+		row("ImagePath") = getImagePath(ImageType.ハサミ)
+		dt.Rows.Add(row)
 
 
-		listBox.ItemsSource = datas
+		listBox.DataContext = dt
+		datagrid.DataContext = dt
 	End Sub
 
 
+	Private Function getImagePath(ByVal type As ImageType) As String
+		Select Case type
+			Case ImageType.ハサミ
+				Return ".\Images\ハサミのフリーアイコン.png"
+			Case ImageType.バット
+				Return ".\Images\バットのアイコン素材.png"
+			Case ImageType.温泉宿
+				Return ".\Images\温泉宿の暖簾アイコン.png"
+			Case ImageType.夜空
+				Return ".\Images\夜空のフリーアイコン.png"
+		End Select
+
+		Return ""
+	End Function
+
 End Class
+
+Public Enum ImageType
+	ハサミ = 0
+	バット = 1
+	温泉宿 = 2
+	夜空 = 3
+	None = 4
+End Enum
